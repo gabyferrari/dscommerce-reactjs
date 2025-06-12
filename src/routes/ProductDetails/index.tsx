db84a85162d8a03a497c1e922a8e7f9683255f3f
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-06-12 16:31:58 -0300

    Passing a product object as a Prop

--- src/routes/ProductDetails/index.tsx
+++ src/routes/ProductDetails/index.tsx
@@ -4,6 +4,25 @@ import ButtonInverse from "../../components/ButtonInverse";
 import ButtonPrimary from "../../components/ButtonPrimary";
 import HeaderClient from "../../components/HeaderClient";
 import ProductDetailsCard from "../../components/ProductDetailsCard";
+import type { ProductDTO } from '../../models/product';
+
+const product: ProductDTO = {
+    id: 2,
+    name: "Smart TV",
+    description: "Esta TV é muito bonita",
+    imgUrl: "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/refs/heads/master/backend/img/2-big.jpg",
+    price: 2500.99,
+    categories: [
+        {
+          id: 2,
+          name: "Eletrônicos"
+        },
+        {
+          id: 3,
+          name: "Computadores"
+        }    
+    ]
+}
 
 export default function ProductDetails() {
 
@@ -12,7 +31,7 @@ export default function ProductDetails() {
         <HeaderClient />
         <main>
             <section id="product-details-section" className="dsc-container">
-                <ProductDetailsCard />
+                <ProductDetailsCard product={product} />
                  <div className="dsc-btn-page-container">
                     <ButtonPrimary />
                     <ButtonInverse />
