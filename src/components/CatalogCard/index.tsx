a7e7942c5dec5b05cc6d144c8a736b55155a5d56
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-06-18 16:22:47 -0300

    Dynamic product details with route parameter

--- src/components/CatalogCard/index.tsx
+++ src/components/CatalogCard/index.tsx
@@ -1,5 +1,6 @@
 import "./styles.css";
 import type { ProductDTO } from "../../models/product";
+import { Link } from "react-router-dom";
 
 type Props = {
   product: ProductDTO;
@@ -8,7 +9,8 @@ type Props = {
 export default function CatalogCard({product} : Props) {
 
   return (
-    <div className="dsc-card">
+    <Link to={`/product-details/${product.id}`}>
+      <div className="dsc-card">
       <div className="dsc-catalog-card-top dsc-line-bottom">
         <img src={product.imgUrl} alt={product.name} />
       </div>
@@ -16,6 +18,7 @@ export default function CatalogCard({product} : Props) {
         <h3>R$ {product.price.toFixed(2)}</h3>
         <h4>{product.name}</h4>
       </div>
-    </div>
+     </div>
+    </Link>
   );
 }
