cc0ddfd1e5e2d64fe432e00051d1cbc8efec53d4
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-06-27 16:42:48 -0300

    First request test with Axios

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -2,15 +2,26 @@ import "./styles.css";
 import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
-import * as productService from '../../../services/product-service'
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import type { ProductDTO } from "../../../models/product";
+import axios from "axios";
 
 export default function ProductDetails() {
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const params = useParams();
 
-  const product = productService.findById(Number(params.productId));
+  const [product, setProduct] = useState<ProductDTO>();
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/products/1")
+    .then(response => {
+      console.log(response.data);
+      setProduct(response.data);
+    })
+  }, []);
 
   return (
     <main>
