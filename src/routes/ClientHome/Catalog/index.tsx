32b260c96c4fdf0809d81c4ea4ac527d00b5705a
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-06-30 12:52:41 -0300

    Organizing project code

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -2,16 +2,16 @@ import './styles.css'
 import SearchBar from '../../../components/SearchBar';
 import CatalogCard from '../../../components/CatalogCard';
 import ButtonNextPage from '../../../components/ButtonNextPage';
+import * as productService from '../../../services/product-service';
 import { useEffect, useState } from 'react';
 import type { ProductDTO } from '../../../models/product';
-import axios from 'axios';
 
 export default function Catalog() {
 
   const [products, setProducts] = useState<ProductDTO[]>([]);
 
   useEffect(() => {
-    axios.get("http://localhost:8080/products?size=12")
+    productService.findAll()
     .then(response => {
       setProducts(response.data.content);
     });
