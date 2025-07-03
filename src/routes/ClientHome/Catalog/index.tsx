e0e9c7f22303667ee45cbe713fc0c6c7dd02a033
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-07-03 17:06:41 -0300

    First test with LocalStorage

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -5,16 +5,29 @@ import ButtonNextPage from '../../../components/ButtonNextPage';
 import * as productService from '../../../services/product-service';
 import { useEffect, useState } from 'react';
 import type { ProductDTO } from '../../../models/product';
+import type { CategoryDTO } from '../../../models/category';
 
 export default function Catalog() {
 
   const [products, setProducts] = useState<ProductDTO[]>([]);
 
+  const objTest : CategoryDTO = {
+    id: 8,
+    name: "Jardinagem"
+  }
+
   useEffect(() => {
+
+    localStorage.setItem("minhaCategoria", JSON.stringify(objTest)); //envia o objTest para o LocalStorage
+
+    const obj = JSON.parse(localStorage.getItem("minhaCategoria") || "{}"); //pega o obj do LocalStorage e imprime no console com o console.log
+    console.log(obj);
+
     productService.findAll()
     .then(response => {
       setProducts(response.data.content);
     });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
     
   return (
