73ff1ee6317684201938ccf63954f04edf0cdbb8
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-07-08 17:07:42 -0300

    SearchBar onSearch event

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,13 +1,38 @@
-
+import { useState } from 'react';
 import './styles.css'
 
-export default function SearchBar() {
+type Props = {
+  onSearch: (query: string) => void;
+}
+
+export default function SearchBar({onSearch} : Props) {
+
+  const [text, setText] = useState("");
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    onSearch(text);
+  }
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setText(event.target.value);
+  }
+
+  function handleResetClick() {
+    setText("");
+    onSearch(text);
+  }
 
   return (
-    <form className="dsc-search-bar">
+    <form className="dsc-search-bar" onSubmit={handleSubmit}>
       <button type="submit">🔎︎</button>
-      <input type="text" placeholder="Nome do produto" />
-      <button type="reset">🗙</button>
+      <input 
+        value={text}
+        type="text" 
+        placeholder="Nome do produto" 
+        onChange={handleChange}
+      />
+      <button onClick={handleResetClick}>🗙</button>
     </form>
   );
 }
