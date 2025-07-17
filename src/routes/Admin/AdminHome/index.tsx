bb504536944848cbe348127aebeac226c91a27cf
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-07-17 13:06:21 -0300

    Getting logged user from backend

--- src/routes/Admin/AdminHome/index.tsx
+++ src/routes/Admin/AdminHome/index.tsx
@@ -1,12 +1,26 @@
+import { useEffect, useState } from "react";
 import "./styles.css";
+import type { UserDTO } from "../../../models/user";
+import * as userService from "../../../services/user-service";
 
 export default function AdminHome() {
+  const [user, setUser] = useState<UserDTO>();
+
+  useEffect(() => {
+    userService.findMe().then((response) => {
+      setUser(response.data);
+      console.log(response.data);
+    })
+    .catch(error => {
+        console.log("Error", error)
+    })
+  }, []);
 
   return (
     <main>
       <section id="admin-home-section" className="dsc-container">
         <h2 className="dsc-section-title dsc-mb20">
-          Bem-vindo à área administrativa
+          Bem-vindo à área administrativa {user?.name}
         </h2>
       </section>
     </main>
