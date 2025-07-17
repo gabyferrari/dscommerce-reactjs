dd69b5c71b36edebc8df618f3e49288399aafca2
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-07-17 15:05:38 -0300

    Decoding the token to obtain the payload with jwt-decode

--- src/models/auth.ts
+++ src/models/auth.ts
@@ -1,4 +1,12 @@
+export type RoleEnum = "ROLE_CLIENT" | "ROLE_ADMIN";
+
 export type CredentialsDTO = {
-    username: string;
-    password: string;
-}
+  username: string;
+  password: string;
+};
+
+export type AccessTokenPayloadDTO = {
+  exp: number;
+  username: string;
+  authorities: RoleEnum[];
+};
--- src/routes/ClientHome/Login/index.tsx
+++ src/routes/ClientHome/Login/index.tsx
@@ -15,8 +15,8 @@ export default function Login() {
         event.preventDefault();
         authService.loginRequest(formData)
             .then(response => {
-                authService.saveAccessToken(response.data.access_token)
-                console.log(response.data);
+                authService.saveAccessToken(response.data.access_token);
+                console.log(authService.getAccessTokenPayload()?.username);
             })
             .catch(error => {
                 console.log("Erro no login", error);
