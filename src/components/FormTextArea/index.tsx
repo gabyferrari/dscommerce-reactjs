6e625ef749721e5233999edf2d989261ec16b74d
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-07-29 16:44:18 -0300

    Customized FormTextArea

---
+++ src/components/FormTextArea/index.tsx
@@ -0,0 +1,26 @@
+export default function FormTextArea(props: any) {
+  const { 
+    validation, 
+    invalid = "false", 
+    dirty = "false", 
+    onTurnDirty, 
+    ...textareaProps 
+  } = props;
+
+  function handleBlur(e: React.FocusEvent<HTMLInputElement>) {
+    if (validation && !validation(e.target.value)) {
+      console.log("Valor inválido");
+    }
+
+    onTurnDirty?.(props.name);
+  }
+
+  return (
+    <textarea 
+        {...textareaProps} 
+        onBlur={handleBlur} 
+        data-invalid={invalid} 
+        data-dirty={dirty}
+    />
+  );
+}
