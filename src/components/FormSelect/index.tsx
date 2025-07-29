53fa945ad6fe76ca25e9be09110e7390f26efd6a
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-07-29 17:31:33 -0300

    React Select integration

---
+++ src/components/FormSelect/index.tsx
@@ -0,0 +1,33 @@
+import Select from "react-select";
+
+export default function FormSelect(props: any) {
+  const {
+    className, 
+    validation, 
+    invalid = "false", 
+    dirty = "false", 
+    onTurnDirty, 
+    ...selectProps 
+  } = props;
+
+  function handleBlur(e: React.FocusEvent<HTMLInputElement>) {
+    if (validation && !validation(e.target.value)) {
+      console.log("Valor inválido");
+    }
+
+    onTurnDirty?.(props.name);
+  }
+
+  return (
+    <div
+      className={className}
+      data-invalid={invalid} 
+      data-dirty={dirty}
+    >
+      <Select 
+        {...selectProps} 
+        onBlur={handleBlur} 
+      />
+    </div>
+  );
+}
--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -4,13 +4,18 @@ import { useEffect, useState } from 'react';
 import FormInput from '../../../components/FormInput';
 import * as forms from '../../../utils/forms';
 import * as productService from '../../../services/product-service';
+import * as categoryService from '../../../services/category-service';
 import FormTextArea from '../../../components/FormTextArea';
+import type { CategoryDTO } from '../../../models/category';
+import FormSelect from '../../../components/FormSelect';
 
 export default function ProductForm() {
 
   const params = useParams();
 
   const isEditing = params.productId !== 'create';
+
+  const [categories, setCategories] = useState<CategoryDTO[]>([]);
 
   const [formData, setFormData] = useState<any>({
     name: {
@@ -53,7 +58,24 @@ export default function ProductForm() {
       },
       message: "A descrição deve ter pelo menos 10 caracteres"
     },
+    categories: {
+      value: [],
+      id: "categories",
+      name: "categories",
+      placeholder: "Categorias",
+      validation: function(value: CategoryDTO[]) {
+        return value.length > 0;
+      },
+      message: "Escolha ao menos 1 categoria"
+    }
   });
+
+  useEffect(() => {
+    categoryService.findAllRequest()
+      .then(response => {
+        setCategories(response.data);
+      });
+  }, );
 
   useEffect(() => {
     if (isEditing) {
@@ -61,9 +83,9 @@ export default function ProductForm() {
         .then(response => {
           const newFormData = forms.updateAll(formData, response.data)
           setFormData(newFormData);
-        })
+        });
     }
-  }, )
+  }, );
 
   function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
     setFormData(forms.updateAndValidate(formData, event.target.name, event.target.value));
@@ -106,6 +128,22 @@ export default function ProductForm() {
                   onTurnDirty={handleTurnDirty}
                   onChange={handleInputChange}
                 />
+              </div>
+              <div>
+                <FormSelect
+                  { ...formData.categories }
+                  className="dsc-form-control"
+                  options={categories} 
+                  onChange={(obj: any) => {
+                    const newFormData = forms.updateAndValidate(formData, "categories", obj);
+                    setFormData(newFormData);
+                  }}
+                  onTurnDirty={handleTurnDirty}
+                  isMulti
+                  getOptionLabel={(obj: any) => obj.name}
+                  getOptionValue={(obj: any) => String(obj.id)}
+                />
+                <div className="dsc-form-error">{formData.categories.message}</div>
               </div>
               <div>
                 <FormTextArea
